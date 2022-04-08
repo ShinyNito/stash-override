a468646c93ec6bd78bfc44dd42b758e77da49ff4
ShinyNito <82853247+ShinyNito@users.noreply.github.com>
2022-04-08 15:52:50 +0800

    Create smzdm_remove_ads.js

---
+++ smzdm_remove_ads.js
@@ -0,0 +1,10 @@
+let obj = JSON.parse($response.body);
+obj.data.forEach((element) => {
+  element.start_date = "2030-12-24 00:00:00";
+  element.end_date = "2030-12-24 23:59:59";
+  element.unix_start_date = "1924272000";
+  element.unix_end_date = "1924358399";
+  element.is_show_ad = "0";
+});
+response = { body: JSON.stringify(obj) };
+$done(response);
